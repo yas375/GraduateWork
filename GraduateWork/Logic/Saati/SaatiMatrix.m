b7b458b09367cae196fc0fa183a36d75e8677a3e
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2013-05-19 19:24:32 +0300

    Implemented SaatiMatrix

--- GraduateWork/Logic/Saati/SaatiMatrix.m
+++ GraduateWork/Logic/Saati/SaatiMatrix.m
@@ -2,14 +2,29 @@
 // Created by Victor Ilyukevich on 06.05.13.
 // Copyright (c) 2013 Victor Ilyukevich. All rights reserved.
 //
-// To change the template use AppCode | Preferences | File Templates.
-//
-
 
 #import "SaatiMatrix.h"
 
+@implementation SaatiMatrix
 
-@implementation SaatiMatrix {
+#pragma mark - Matrix
 
+- (void)setSize:(NSInteger)size
+{
+  [super setSize:size];
+  for (int i = 0; i < size; i++) {
+    [super setValue:[Fraction fractionWithNumerator:1] forRow:i column:i];
+  }
 }
+
+- (void)setValue:(Fraction *)value forRow:(NSUInteger)row column:(NSUInteger)column
+{
+  if (row == column) {
+    [NSException raise:@"Don't touch main diagonal" format:@"Attempt to set %@ on (%d,%d)", value, row, column];
+  }
+
+  [super setValue:value forRow:row column:column];
+  [super setValue:[value reversedFraction] forRow:column column:row];
+}
+
 @end
--- GraduateWorkTests/Logic/Saati/SaatiMatrixSpec.m
+++ GraduateWorkTests/Logic/Saati/SaatiMatrixSpec.m
@@ -2,7 +2,29 @@
 #import "SaatiMatrix.h"
 
 SPEC_BEGIN(SaatiMatrixSpec)
-        describe(@"SaatiMatrix spec", ^{
 
-        });
+describe(@"SaatiMatrix", ^{
+  context(@"with size 4", ^{
+    __block SaatiMatrix *matrix = nil;
+    beforeEach(^{
+      matrix = [[SaatiMatrix alloc] initWithSize:4];
+    });
+    it(@"returns 1 for values on main diagonal", ^{
+      [[[matrix valueForRow:0 column:0] should] equal:[Fraction fractionWithNumerator:1]];
+      [[[matrix valueForRow:1 column:1] should] equal:[Fraction fractionWithNumerator:1]];
+      [[[matrix valueForRow:2 column:2] should] equal:[Fraction fractionWithNumerator:1]];
+      [[[matrix valueForRow:3 column:3] should] equal:[Fraction fractionWithNumerator:1]];
+    });
+    it(@"inserts reversed value as well", ^{
+      [matrix setValue:[Fraction fractionWithNumerator:9] forRow:2 column:3];
+      [[[matrix valueForRow:3 column:2] should] equal:[Fraction fractionWithNumerator:1 denominator:9]];
+    });
+    it(@"raises an exception on attempt to set value for on main diagonal", ^{
+      [[theBlock(^{
+        [matrix setValue:[Fraction fractionWithNumerator:9] forRow:2 column:2];
+      }) should] raise];
+    });
+  });
+});
+
 SPEC_END
